5277d7b76e004a4a7b4c36de666a6601925e3f62
codeandroid <xiehui0902@163.com>
2018-09-12 10:18:16 +0800

    BottomNavigationBar-Corpertino

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:async';
-
+import 'package:flutter_app/home_bottom_navigation.dart';
+import 'package:flutter_app/cupertino/cupertino_navigation.dart';
 void main() => runApp(new FlutterView());
 
 class FlutterView extends StatelessWidget {
@@ -10,72 +11,63 @@ class FlutterView extends StatelessWidget {
     return new MaterialApp(
       title: 'Flutter View',
       theme: new ThemeData(primarySwatch: Colors.green),
-      home: new MyHomePage(),
+      home: new SplashPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class SplashPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  SplashPageState createState() => new SplashPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class SplashPageState extends State<SplashPage> {
   static const String _channel = 'increment';
   static const String _pong = 'pong';
   static const String _emptyMessage = '';
   static const BasicMessageChannel<String> platform =
       BasicMessageChannel<String>(_channel, StringCodec());
   int _counter = 0;
+  Timer timer;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    platform.setMockMessageHandler(_handlePlatformIncrement);
-  }
-
-  Future<String> _handlePlatformIncrement(String message) async {
-    setState(() {
-      _counter++;
+    timer = new Timer(new Duration(milliseconds: 1500), () {
+      Navigator.of(context).pushAndRemoveUntil(new MaterialPageRoute(builder: (context) => new CupertinoNavigation()
+//      Navigator.of(context).pushAndRemoveUntil(new MaterialPageRoute(builder: (context) => new HomeTab()
+      ), (route) => route == null);
     });
-    return _emptyMessage;
   }
+  
 
-  void _sendFlutterIncrement() {
-    platform.send(_pong);
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Column(
+    return new Material(
+      color: Color(0xffffffff),
+      child: new Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          new Expanded(
-              child: new Center(
-            child: new Text(
-                'Platform button tapped $_counter time ${_counter == 1 ? '' : 's'}.'),
-          )),
-          new Container(
-            padding: const EdgeInsets.only(bottom: 15.0, left: 5.0),
-            child: new Row(
-              children: <Widget>[
-//                assets文件在yaml中指明
-                new Image.asset('assets/flutter-mark-square-64.png',
-                    scale: 1.5),
-                const Text(
-                  'Flutter',
-                  style: TextStyle(fontSize: 30.0),
-                )
-              ],
-            ),
+          Text(
+            'Flutter Demo',
+            style: TextStyle(
+                color: Color(0xff70C2F0),
+                fontWeight: FontWeight.bold,
+                fontSize: 26.0),
+          ),
+          new Padding(padding: EdgeInsets.only(top: 30.0)),
+          new Image.asset(
+            'assets/flutter-mark-square-64.png',
           )
         ],
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _sendFlutterIncrement,
-        child: const Icon(Icons.add),
       ),
     );
   }
